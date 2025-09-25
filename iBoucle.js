fcf37cc01b29df76f3bea5462d00acf6e4799cbd
fadaldrame97-sys <fadaldrame97@gmail.com>
2025-09-25 10:14:41 +0200

    Add files via upload

--- iBoucle.js
+++ iBoucle.js
@@ -1,3 +1,11 @@
-for (let i =0; i <= 5;i++){
-    console.log(i)
+const prompt = require("prompt-sync")();
+let nombre = prompt("donner un nombre");
+
+
+
+
+
+for (let i = 1; i <= 10;i++){
+    let total= nombre * i;
+    console.log(total);
 }
